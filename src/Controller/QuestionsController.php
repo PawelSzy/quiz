6f1d2773a6cfabccc3d6923163995108911f8b43
PawelSzy <peal1982@gmail.com>
2019-12-05 14:07:23 +0100

    Create crud and fix look of pagination

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,18 +18,10 @@ class QuestionsController extends AbstractController
      */
     public function index(QuestionRepository $questionRepository, PaginatorInterface $paginator,  Request $request): Response
     {
-//        var_dump($questionRepository->findAll());
-
-
-//        $repository = $this->getDoctrine()->getRepository(Blog::class);
         $pagination = $paginator->paginate($questionRepository->findAll(),$request->query->getInt('page', 1),10);
 
         return $this->render('questions/index.html.twig', [
             'pagination' => $pagination,
         ]);
-
-//        return $this->render('questions/index.html.twig', [
-//            'controller_name' => 'QuestionsController',
-//        ]);
     }
 }
---
+++ src/Form/QuestionType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Question;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content')
+            ->add('active')
+            ->add('created')
+            ->add('questionLevel')
+            ->add('sigleOrMulti')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Question::class,
+        ]);
+    }
+}
